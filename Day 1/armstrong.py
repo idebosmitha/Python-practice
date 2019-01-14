1f402879c810472e4de49b101ee60555997d6170
iDebosmitha <idebosmitha@gmail.com>
2019-01-14 13:34:03 +0530

    completed armstrong.py

---
+++ Day 1/armstrong.py
@@ -0,0 +1,12 @@
+def sum_of_digits(x):
+	no_of_digits = len(str(x))
+	sum_ = 0
+	while x > 0:
+		digit = x % 10
+		x //= 10
+		sum_ += pow(digit, no_of_digits)
+	return sum_
+
+n = int(input('Enter a number: '))
+s = sum_of_digits(n)
+print(n == s)
