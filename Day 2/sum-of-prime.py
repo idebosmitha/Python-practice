67ad20931b8ba40325a32f9b36fff728804ee846
iDebosmitha <idebosmitha@gmail.com>
2019-01-16 20:54:45 +0530

    added sum of primes

---
+++ Day 2/sum-of-prime.py
@@ -0,0 +1,25 @@
+from functools import reduce
+
+
+def is_prime(x):
+	if x == 1:
+		return False
+	for i in range(2, x//2 + 1):
+		if x % i == 0:
+			return False
+	return True
+
+
+n1 = int(input('Enter the value of n1'))
+n2 = int(input('Enter the value of n2'))
+sum_of_primes = 0
+
+for i in range(n1, n2 + 1):
+	if is_prime(i):
+		sum_of_primes += i
+
+print(sum_of_primes)
+
+
+# Do not read if you're a beginner
+print(reduce(lambda a, b: a + b, filter(is_prime, list(range(n1, n2+1)))))
